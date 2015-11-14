d6a557c4948ca3fcd7877f5bfed5933243f9e053
Daniel Eriksson <daniel@deriksson.se>
2015-11-14 13:56:56 -0800

    Add command line interface for Pet class
    A very simple command line interface for testing out the Pet class. Every
    time you hit enter, the increment_time method will be called and the
    interact_with method will be called with the text you have typed since
    last time as the argument. If you type 'quit' the program will stop. An
    empty string will not stop the program.

---
+++ pet_cli.py
@@ -0,0 +1,19 @@
+"""Command Line Interface for the Pet class.
+Can be used as a demonstration of the class or
+as a convenient way to test new functionality.
+"""
+
+from pet import Pet
+
+
+if __name__ == '__main__':
+    p = Pet()
+    print 'Type something to interact or type "quit" to quit.'
+
+    while True:
+        interation = raw_input()
+        if interation.lower() == 'quit':
+            break
+
+        p.increment_time()
+        print p.interact_with(interation)
